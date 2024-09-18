160905814e132333d8ef78dd5315c4914d36d060
skweel <dissingardjino@icloud.com>
2024-09-18 14:58:15 +0200

    first commit

---
+++ b3devgr2/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  b3devgr2
+//
+//  Created by Djino Dissingar on 18/09/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
