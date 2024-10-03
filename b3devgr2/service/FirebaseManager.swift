39f0835dd493ee48c6194789178cee169a8f815e
skweel <dissingardjino@icloud.com>
2024-10-03 17:01:26 +0200

    new mise en place

---
+++ b3devgr2/service/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  b3devgr2
+//
+//  Created by Djino Dissingar on 03/10/2024.
+//
+
+import Foundation
---
+++ b3devgr2/view/DashBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashBoardView.swift
+//  b3devgr2
+//
+//  Created by Djino Dissingar on 03/10/2024.
+//
+
+import SwiftUI
+
+struct DashBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashBoardView()
+}
